f0676d7062a5d13fc056f150b1fc272f5d1eedd9
Matheus Ferreira <matheusjuan.f@outlook.com>
2025-11-21 18:10:51 -0300

    ➕ build: add Koin dependency for dependency injection and include domain module

--- fiserv/msitef/build.gradle.kts
+++ fiserv/msitef/build.gradle.kts
@@ -34,6 +34,10 @@ android {
 
 dependencies {
 
+    implementation(project(":domain"))
+
+    implementation(libs.koin.android)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
