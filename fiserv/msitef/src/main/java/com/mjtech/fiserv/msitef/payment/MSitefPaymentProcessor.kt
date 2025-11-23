1e0ed3a351b7347ce23b96d4b03ae4622263f44a
Matheus Ferreira <matheusjuan.f@outlook.com>
2025-11-23 19:05:50 -0300

    ✨ feat: add support for PIX payment type in MSitef payment processor

--- fiserv/msitef/src/main/java/com/mjtech/fiserv/msitef/payment/MSitefPaymentProcessor.kt
+++ fiserv/msitef/src/main/java/com/mjtech/fiserv/msitef/payment/MSitefPaymentProcessor.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import com.mjtech.fiserv.msitef.common.ACESSIBILIDADE_VISUAL
+import com.mjtech.fiserv.msitef.common.CD_HABILITADAS
 import com.mjtech.fiserv.msitef.common.CNPJ_CPF
 import com.mjtech.fiserv.msitef.common.EMPRESA_SITEF
 import com.mjtech.fiserv.msitef.common.ENDERECO_SITEF
@@ -54,6 +55,12 @@ internal class MSitefPaymentProcessor(private val context: Context) : PaymentPro
                 putExtra("numParcelas", payment.installmentDetails?.installments.toString())
             }
 
+            // Parâmetros específicos para o PIX
+            if (payment.type == PaymentType.PIX) {
+                putExtra("transacoesHabilitadas", "7;8;")
+                putExtra("cnpj_automacao", CNPJ_CPF)
+            }
+
             // Confiugurações adicionais
             putExtra("acessibilidadeVisual", ACESSIBILIDADE_VISUAL)
             putExtra("timeoutColeta", TIMEOUT_COLETA)
@@ -77,15 +84,19 @@ internal class MSitefPaymentProcessor(private val context: Context) : PaymentPro
 
     /** Mapeia as restrições de transação com base no tipo de pagamento e parcelamento */
     private fun mapRestriction(method: PaymentType, installment: Int): String {
+        val restrictionType =
+            if (method == PaymentType.PIX) "CarteirasDigitaisHabilitadas"
+            else "TransacoesHabilitadas"
         val restrictionCode = when (method) {
             PaymentType.DEBIT -> "16"
             PaymentType.CREDIT -> {
                 if (installment > 1) "27" else "26"
             }
 
+            PaymentType.PIX -> CD_HABILITADAS
             else -> "0"
         }
-        return "TransacoesHabilitadas=$restrictionCode"
+        return "$restrictionType=$restrictionCode"
     }
 
     companion object {
